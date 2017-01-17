4a974396e03764ed98df0ee1693f30b8d0544f7c
Travis Kohlbeck <tdkohlbeck+github@gmail.com>
2017-01-16 17:34:14 -0800

    static array cyclic queue

--- practice/Jan1617.java
+++ practice/Jan1617.java
@@ -47,16 +47,71 @@ class CLL<T> {
 	}
 }
 
+class Q<T> {
+	private Object[] arr;
+	private int front, back, curr, cap;
+	public Q(int size) {
+		front = 0;
+		back = 0;
+		curr = 0;
+		cap = size;
+		arr = new Object[size];
+	}
+	public void enq(T data) {
+		if (curr == cap) throw new Error("at capacity");
+		back = (front + curr) % cap;
+		arr[back] = (T) data;
+		curr++;
+	}
+	public T deq() {
+		if (curr == 0) throw new Error("ain\'t nothing there");
+		@SuppressWarnings("unchecked")
+		T data = (T) arr[front];
+		arr[front] = null;
+		front = (front + 1) % cap;
+		curr--;
+		return data;
+	}
+	public String toString() {
+		StringBuffer s = new StringBuffer();
+		int i = 0;
+		while (i < 4) {
+			int next = (front + i) % cap;
+			s.append(arr[next] + ", ");
+			i++;
+		}
+		return s.toString();
+	}
+}
+
 public class Jan1617<T> {
-	public void testCLL() {
+	public static void testCLL() {
 		CLL<String> list = new CLL<String>();
 		list.add("eh");
 		list.add("real");
 		list.add("dolphins");
 		System.out.println(list.toString());
 	}
+	public static void testQ() {
+		Q<Integer> q = new Q<Integer>(4);
+		q.enq(1);
+		System.out.println(q.toString());
+		q.enq(2);
+		System.out.println(q.toString());
+		q.enq(3);
+		System.out.println(q.toString());
+		q.enq(4);
+		System.out.println(q.toString());
+		q.deq();
+		System.out.println(q.toString());
+		q.deq();
+		System.out.println(q.toString());
+		q.enq(9);
+		System.out.println(q.toString());
+	}
 	public static void main(String[] args) {
-		testCLL();
+		//testCLL();
+		testQ();
 	}
 }
 /*
@@ -64,4 +119,7 @@ LESSONS:
 * make sure there"s a list when calling toString()
 * double quotes, not singles!
 * adding first cyclic, you have to have head point to something before instantiating first node (so that you can pass that pointer to the node)
+* make sure methods return something when they say they will
+* a static method can only call other static methods... makes sense.
+* increment and decrement curr when calling enq and deq!!
 */
